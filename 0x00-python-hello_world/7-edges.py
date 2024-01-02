a85c68c57ad36415569d2a45b3a56419d87d0a86
root <kbthaba@gmail.com>
2024-01-02 08:07:53 -0800

    7.edges

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(word[0:3])
-print(word[-2:])
-print(word[1:-1])
+print("First 3 letters:", word[0:3])
+print("Last 2 letters:", word[-2:])
+print("Middle word:", word[1:-1])
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
