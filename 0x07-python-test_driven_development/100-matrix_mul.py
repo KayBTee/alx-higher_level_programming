09009e85c7610c0218fdd92c8713367653fb5038
KayBTee <kbthaba@gmail.com>
2024-02-01 10:39:23 +0000

    100matrix mul

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines a matrix multiplication function."""
 
-
 def matrix_mul(m_a, m_b):
     """Multiply two matrices.
 
@@ -10,7 +9,7 @@ def matrix_mul(m_a, m_b):
         m_b (list of lists of ints/floats): The second matrix.
     Raises:
         TypeError: If either m_a or m_b is not a list of lists of ints/floats.
-        TypeError: If either m_a or m_b is empty.
+        ValueError: If either m_a or m_b is empty.
         TypeError: If either m_a or m_b has different-sized rows.
         ValueError: If m_a and m_b cannot be multiplied.
     Returns:
@@ -40,9 +39,9 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_b should contain only integers or floats")
 
     if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must should be of the same size")
+        raise TypeError("each row of m_a must be of the same size")
     if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must should be of the same size")
+        raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
@@ -65,4 +64,4 @@ def matrix_mul(m_a, m_b):
         new_matrix.append(new_row)
 
     return new_matrix
-
+```
