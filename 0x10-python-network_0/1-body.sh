23cea63231229dd9d789eeb293d3463ec8b1ae8d
KayBTee <kbthaba@gmail.com>
2024-08-29 07:46:49 -0700

    body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a GET request to the
-# URL, and displays the body of the response
-curl -Ls "$1"
+# Write a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
